c664d786d1ea71450f4f7c171a4fde0fc17978a8
GrimyoW <pretki.patryk@gmail.com>
2021-03-07 13:12:46 +0100

    Drugi push

--- src/main/java/patryk/pretki/com/rentcar/RentalOffice/Return.java
+++ src/main/java/patryk/pretki/com/rentcar/RentalOffice/Return.java
@@ -31,5 +31,62 @@ public class Return {
   @Column(name = "uwagi")
   private String uwagi;
 
+  public Return(Customers customers, LocalDate returnDate, Reservation reservation, BigDecimal suplemnet, String uwagi) {
+    this.customers = customers;
+    this.returnDate = returnDate;
+    this.reservation = reservation;
+    this.suplemnet = suplemnet;
+    this.uwagi = uwagi;
+  }
 
+  public Customers getCustomers() {
+    return customers;
+  }
+
+  public void setCustomers(Customers customers) {
+    this.customers = customers;
+  }
+
+  public LocalDate getReturnDate() {
+    return returnDate;
+  }
+
+  public void setReturnDate(LocalDate returnDate) {
+    this.returnDate = returnDate;
+  }
+
+  public Reservation getReservation() {
+    return reservation;
+  }
+
+  public void setReservation(Reservation reservation) {
+    this.reservation = reservation;
+  }
+
+  public BigDecimal getSuplemnet() {
+    return suplemnet;
+  }
+
+  public void setSuplemnet(BigDecimal suplemnet) {
+    this.suplemnet = suplemnet;
+  }
+
+  public String getUwagi() {
+    return uwagi;
+  }
+
+  public void setUwagi(String uwagi) {
+    this.uwagi = uwagi;
+  }
+
+  @Override
+  public String toString() {
+    return "Return{" +
+      "customers=" + customers +
+      ", returnDate=" + returnDate +
+      ", reservation=" + reservation +
+      ", suplemnet=" + suplemnet +
+      ", uwagi='" + uwagi + '\'' +
+      '}';
+  }
 }
